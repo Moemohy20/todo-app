9e4d80da4ddc5090d4f08791cec519417e0fa7fa
Mahmoud Mohy <moe.mohy@outlook.com>
2023-03-15 21:54:26 +0200

    Empty-Commit

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,6 @@ async function bootstrap() {
     .setTitle('Todo App')
     .setDescription('The Todo API description')
     .setVersion('1.0')
-    .addTag('Todo Documentations')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
